d71448ba2500fee7a669b72e46dd85c4cac17d47
Charles2205 <cewudzie0@gmail.com>
2023-10-12 19:51:50 +0000

    Update Done

--- assignment-2/src/pages/index.jsx
+++ assignment-2/src/pages/index.jsx
@@ -1,19 +1,24 @@
 const Index = () => {
   return (
     <>
-      <div className="card" style={{width: "18rem;"}}>
+     
+      
+      <div className="d-flex justify-content-center align-items-center  " style={{height: "100vh"}}>
+      <div className="card custom">
+      <div className="card  custom " style={{width: "20rem"}}>
         <img src="../img/image-qr-code.png" className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title ">Improve your Front-end <br /> Skills by building projects</h5>
           <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Scan the QR code to visit Frontend <br /> Mentor and take your coding skills to <br /> the next level
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          
         </div>
       </div>
+      </div>
+      </div>
+      
+     
     </>
   );
 };
